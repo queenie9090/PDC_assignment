de350e81c500be4ed8f21a34c5ded9ff00decdb2
queenie9090 <queenie050306@gmail.com>
2026-08-09 02:49:01 +0800

    small changes

--- PDC_assignment/main.cpp
+++ PDC_assignment/main.cpp
@@ -84,8 +84,6 @@ int main(int argc, char* argv[]) {
     }
     else if (mode == "openmp") {
         resize_image_openmp(out_img.data, img.data, old_w, old_h, new_w, new_h);
-        std::cerr << "Error: OpenMP mode is not implemented yet.\n";
-        return 1;
     }
     else if (mode == "mpi") {
         // Uncomment once resizer_mpi.h is ready
