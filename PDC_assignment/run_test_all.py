a415a07c9c073051e6c92dff262a1bfd4c87d14b
queenie9090 <queenie050306@gmail.com>
2026-08-22 18:25:39 +0800

    add run_test_all and csv files

--- PDC_assignment/run_test_all.py
+++ PDC_assignment/run_test_all.py
@@ -11,7 +11,7 @@ EXE_PATH = r"..\x64\Release\PDC_assignment.exe"
 SCALE_DOWN = 0.5
 SCALE_UP = 1.5
 
-TARGET_BACKEND = "baseline"
+TARGET_BACKEND = "all"
 
 if TARGET_BACKEND == "all":
     MODES = ["baseline", "cuda", "openmp", "mpi"]
